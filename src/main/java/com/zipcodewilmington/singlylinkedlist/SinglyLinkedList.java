0172862df190c74c8839a5aa5749b112e91e3c53
Chung Arguello <chung@zipcoders-MacBook-Pro-90.local>
2019-11-13 08:51:08 -0500

    progressing

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,28 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    private Node head;
+    private Node tail;
+    private static Integer size;
+
+    public SinglyLinkedList(T head){
+       this.head = new Node<T>(head);
+       size = 1;
+    }
+
+    public void add(T data){
+        Node<T> newTail = new Node<T>(data);
+        if(size.equals(1)){
+            head.setNext(newTail);
+            tail = newTail;
+        }
+        else{
+            tail.setNext(newTail);
+            tail = tail.getNext();
+        }
+        size++;
+    }
+
+
 }
